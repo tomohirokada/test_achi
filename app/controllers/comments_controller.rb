455a204b81910f89b4049507f90ee2a41b740f38
岡田朋大 <okadatomohiro@okadatomoodai-no-MacBook-Air.local>
2017-05-22 17:17:59 +0900

    test_achi

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy, :update, :edit]
   def create
-    @comment = current_user.comments.build(comment_params)
+    @comment = current_user.comments.build(comments_params)
     @blog = @comment.blog
     respond_to do |format|
       if @comment.save
@@ -10,23 +11,37 @@ class CommentsController < ApplicationController
         format.html{render :new}
       end
     end
+  end
 
 
+  def edit
+  end
+
+  def update
+  @comment.update(comment_params)
+  redirect_to blog_path(@blog)
+  end
 
 
   def destroy
-    @comment= Comment.find(params[:id])
-    @blog = @comment.blog
-    @comment.destroy
-    redirect_to blog_path(@blog), notice: "コメントを削除しました！"
+    respond_to do |format|
+      if @comment.destroy
+        format.html{redirect_to blog_path(@blog), notice: "コメントを削除しました！"}
+        format.js{render:index}
+      else
+        format.html{render :new}
+      end
+    end
   end
-
-end
-
 
   private
 
-    def comment_params
+    def comments_params
       params.require(:comment).permit(:blog_id, :content)
     end
-end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+      @blog = @comment.blog
+    end
+  end
