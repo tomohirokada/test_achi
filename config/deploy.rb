41ab7c76dd36a4cbb010f6c67d1fa1b368238012
岡田朋大 <okadatomohiro@okadatomoodai-no-MacBook-Air.local>
2017-06-03 16:36:40 +0900

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'test_achi'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/tomohirokada/test_achi.git'
@@ -10,7 +10,7 @@ set :repo_url, 'https://github.com/tomohirokada/test_achi.git'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/test_achi'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
